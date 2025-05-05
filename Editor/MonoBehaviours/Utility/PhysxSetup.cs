3bcbaf94e818d336728d606aa0f94dfa90f17c27
XDay <XDayxizi@163.com>
2025-05-05 08:59:32 +0800

    refactor

--- Editor/MonoBehaviours/Utility/PhysxSetup.cs
+++ Editor/MonoBehaviours/Utility/PhysxSetup.cs
@@ -30,115 +30,29 @@ using UnityEditor;
 
 namespace XDay.UtilityAPI
 {
-    //设置PhysX,可以直接使用PhysX的接口,不受Unity限制
     [ExecuteInEditMode]
     public class PhysxSetup : MonoBehaviour
     {
-        void Awake()
-        {
-            mEngine = this;
 #if UNITY_EDITOR_WIN
-            // Create PxFoundation
-            mAllocatorCallback = new PxDefaultAllocator();
-            mErrorCallback = new PxErrorToExceptionCallback();
-            mFoundation = PxFoundation.create(PxVersion.PX_PHYSICS_VERSION, mAllocatorCallback, mErrorCallback);
-
-            // Setup PVD connection (optional)
-            mPvd = mFoundation.createPvd();
-            mPvdTransport = PxPvdTransport.create("localhost", 5425, 200);
-            if (!mPvd.connect(mPvdTransport, PxPvdInstrumentationFlag.ALL))
-            {
-                mPvd.release();
-                mPvd = null;
-                mPvdTransport.release();
-                mPvdTransport = null;
-            }
-            else
-                Debug.Log("Connected to PVD.");
-
-            // Create PxPhysics
-            mPhysics = mFoundation.createPhysics(PxVersion.PX_PHYSICS_VERSION, new PxTolerancesScale(), mPvd);
-
-            // Create CPU dispatcher
-            mCpuDispatcher = PxDefaultCpuDispatcher.create((uint)SystemInfo.processorCount);
-
-            // Create PxScene
-            var sceneDesc = new PxSceneDesc(mPhysics.getTolerancesScale());
-            sceneDesc.cpuDispatcher = mCpuDispatcher;
-            sceneDesc.filterShader = PxDefaultSimulationFilterShader.function;
-            sceneDesc.gravity = new PxVec3(0, -9.8f, 0);
-            sceneDesc.broadPhaseType = PxBroadPhaseType.ABP;
-            mScene = mPhysics.createScene(sceneDesc);
-            sceneDesc.destroy();
-
-            // Set PVD flags
-            var pvdClient = mScene.getScenePvdClient();
-            if (pvdClient != null)
-                pvdClient.setScenePvdFlags(PxPvdSceneFlag.TRANSMIT_CONTACTS | PxPvdSceneFlag.TRANSMIT_CONSTRAINTS | PxPvdSceneFlag.TRANSMIT_SCENEQUERIES);
-
-            mPhysicsMaterial = mPhysics.createMaterial(0.5f, 0.5f, 0.05f);
-
-            // Create PxCooking
-            mCooking = mFoundation.createCooking(PxVersion.PX_PHYSICS_VERSION, new PxCookingParams(mPhysics.getTolerancesScale()));
+        public static PhysxSetup Setup { get { return m_Setup; } }
+        public PxFoundation Foundation { get { return m_Foundation; } }
+        public PxPhysics Physics { get { return m_Physics; } }
+        public PxMaterial Material { get { return m_PhysicsMaterial; } }
+        public PxScene Scene { get { return m_Scene; } }
+        public PxCooking Cooking { get { return m_Cooking; } }
 #endif
-            //reload之前先清理状态
-            AssemblyReloadEvents.beforeAssemblyReload += BeforeReload;
-        }
-
-        void BeforeReload()
-        {
-            OnDestroy();
-        }
-
-        void OnDestroy()
-        {
-#if UNITY_EDITOR_WIN
-            mScene?.release();
-            mScene = null;
-            mPhysicsMaterial?.release();
-            mPhysicsMaterial = null;
-            mCpuDispatcher?.release();
-            mCpuDispatcher = null;
-            mCooking?.release();
-            mCooking = null;
-            
-            mPhysics?.release();
-            mPhysics = null;
-            mPvd?.release();
-            mPvd = null;
-            mPvdTransport?.release();
-            mPvdTransport = null;
-            mFoundation?.release();
-            mFoundation = null;
-            mErrorCallback?.destroy();
-            mErrorCallback = null;
-            mAllocatorCallback?.destroy();
-            mAllocatorCallback = null;
-#endif
-        }
-
-        void FixedUpdate()
-        {
-#if UNITY_EDITOR_WIN
-            if (mScene != null)
-            {
-                mScene.simulate(Time.fixedDeltaTime);
-                mScene.fetchResults(true);
-            }
-#endif
-        }
 
         public bool Raycast(Vector3 origin, Vector3 dir, out Vector3 pos, out Vector3 normal)
         {
             pos = Vector3.zero;
             normal = Vector3.up;
 #if UNITY_EDITOR_WIN
-            if (mScene.raycast(origin.ToPxVec3(), dir.ToPxVec3(), 100000, mBuffer, PxHitFlag.DEFAULT | PxHitFlag.MESH_BOTH_SIDES))
+            if (m_Scene.raycast(origin.ToPxVec3(), dir.ToPxVec3(), 100000, m_RaycastBuffer, PxHitFlag.DEFAULT | PxHitFlag.MESH_BOTH_SIDES))
             {
-                if (mBuffer.hasBlock)
+                if (m_RaycastBuffer.hasBlock)
                 {
-                    pos = mBuffer.block.position.ToVector3();
-                    normal = mBuffer.block.normal.ToVector3();
+                    pos = m_RaycastBuffer.block.position.ToVector3();
+                    normal = m_RaycastBuffer.block.normal.ToVector3();
                     return true;
                 }
             }
@@ -146,27 +60,106 @@ namespace XDay.UtilityAPI
             return false;
         }
 
+        private void Awake()
+        {
+            m_Setup = this;
 #if UNITY_EDITOR_WIN
-        public static PhysxSetup engine { get { return mEngine; } }
-        public PxFoundation foundation { get { return mFoundation; } }
-        public PxPhysics physics { get { return mPhysics; } }
-        public PxMaterial material { get { return mPhysicsMaterial; } }
-        public PxScene scene { get { return mScene; } }
-        public PxCooking cooking { get { return mCooking; } }
+            m_ErrorCallback = new PxErrorToExceptionCallback();
+            m_AllocatorCallback = new PxDefaultAllocator();
+            m_Foundation = PxFoundation.create(PxVersion.PX_PHYSICS_VERSION, m_AllocatorCallback, m_ErrorCallback);
 
-        PxAllocatorCallback mAllocatorCallback;
-        PxErrorCallback mErrorCallback;
-        PxFoundation mFoundation;
-        PxPhysics mPhysics;
-        PxPvdTransport mPvdTransport;
-        PxPvd mPvd;
-        PxDefaultCpuDispatcher mCpuDispatcher;
-        PxScene mScene;
-        PxCooking mCooking;
-        PxMaterial mPhysicsMaterial;
-        PxRaycastBuffer mBuffer = new PxRaycastBuffer();
+            m_Pvd = m_Foundation.createPvd();
+            m_PvdTransport = PxPvdTransport.create("localhost", 5425, 200);
+            if (!m_Pvd.connect(m_PvdTransport, PxPvdInstrumentationFlag.ALL))
+            {
+                m_Pvd.release();
+                m_Pvd = null;
+                m_PvdTransport.release();
+                m_PvdTransport = null;
+            }
+            else
+                Debug.Log("Connected to PVD.");
+
+            m_Physics = m_Foundation.createPhysics(PxVersion.PX_PHYSICS_VERSION, new PxTolerancesScale(), m_Pvd);
+
+            m_DefaultCpuDispatcher = PxDefaultCpuDispatcher.create((uint)SystemInfo.processorCount);
+
+            var sceneDesc = new PxSceneDesc(m_Physics.getTolerancesScale())
+            {
+                cpuDispatcher = m_DefaultCpuDispatcher,
+                filterShader = PxDefaultSimulationFilterShader.function,
+                gravity = new PxVec3(0, -9.8f, 0),
+                broadPhaseType = PxBroadPhaseType.ABP
+            };
+            m_Scene = m_Physics.createScene(sceneDesc);
+            sceneDesc.destroy();
+
+            var pvdClient = m_Scene.getScenePvdClient();
+            pvdClient?.setScenePvdFlags(PxPvdSceneFlag.TRANSMIT_CONTACTS | PxPvdSceneFlag.TRANSMIT_CONSTRAINTS | PxPvdSceneFlag.TRANSMIT_SCENEQUERIES);
+
+            m_Cooking = m_Foundation.createCooking(PxVersion.PX_PHYSICS_VERSION, new PxCookingParams(m_Physics.getTolerancesScale()));
+            m_PhysicsMaterial = m_Physics.createMaterial(0.5f, 0.5f, 0.05f);
 #endif
-        static PhysxSetup mEngine;
+            AssemblyReloadEvents.beforeAssemblyReload += BeforeReload;
+        }
+
+        private void OnDestroy()
+        {
+#if UNITY_EDITOR_WIN
+            m_Scene?.release();
+            m_Scene = null;
+            m_PhysicsMaterial?.release();
+            m_PhysicsMaterial = null;
+            m_DefaultCpuDispatcher?.release();
+            m_DefaultCpuDispatcher = null;
+            m_Cooking?.release();
+            m_Cooking = null;
+            
+            m_Physics?.release();
+            m_Physics = null;
+            m_Pvd?.release();
+            m_Pvd = null;
+            m_PvdTransport?.release();
+            m_PvdTransport = null;
+            m_Foundation?.release();
+            m_Foundation = null;
+            m_AllocatorCallback?.destroy();
+            m_AllocatorCallback = null;
+            m_ErrorCallback?.destroy();
+            m_ErrorCallback = null;
+#endif
+        }
+
+        private void FixedUpdate()
+        {
+#if UNITY_EDITOR_WIN
+            if (m_Scene != null)
+            {
+                m_Scene.simulate(Time.fixedDeltaTime);
+                m_Scene.fetchResults(true);
+            }
+#endif
+        }
+
+        private void BeforeReload()
+        {
+            OnDestroy();
+        }
+
+#if UNITY_EDITOR_WIN
+        private PxPhysics m_Physics;
+        private PxFoundation m_Foundation;
+        private PxPvdTransport m_PvdTransport;
+        private PxRaycastBuffer m_RaycastBuffer = new PxRaycastBuffer();
+        private PxAllocatorCallback m_AllocatorCallback;
+        private PxDefaultCpuDispatcher m_DefaultCpuDispatcher;
+        private PxCooking m_Cooking;
+        private PxMaterial m_PhysicsMaterial;
+        private PxScene m_Scene;
+        private PxErrorCallback m_ErrorCallback;
+        private PxPvd m_Pvd;
+#endif
+        private static PhysxSetup m_Setup;
     }
 }
 
